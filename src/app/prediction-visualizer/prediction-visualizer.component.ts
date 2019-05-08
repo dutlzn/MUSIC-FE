d89dacd79ca7ef02a96f87dfbaab9b856b4b0218
dutlzn <2436013662@qq.com>
2019-05-09 07:46:59 +0800

    添加前端读取流派信息

--- src/app/prediction-visualizer/prediction-visualizer.component.ts
+++ src/app/prediction-visualizer/prediction-visualizer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-prediction-visualizer',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prediction-visualizer.component.css']
 })
 export class PredictionVisualizerComponent implements OnInit {
-
-  constructor() { }
+  datainfo: any;
+  constructor(
+    private httpClient: HttpClient,
+  ) { }
 
   ngOnInit() {
+    this.httpClient.get('http://127.0.0.1:5000/genres_info').subscribe(data => {
+      console.log(data);
+      console.log(data['genres']);
+      this.datainfo = data['genres'];
+    })
   }
+
+  // geners_info() {
+  //   this.httpClient.get('http://127.0.0.1:5000/genres_info').subscribe(data => {
+  //     console.log(data);
+  //     console.log(data['genres']);
+  //     this.datainfo = data['genres'];
+  //   })
+  // }
 
 }
